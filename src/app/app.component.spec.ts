1c6dcac3cf1b3aa0d490452b07c18da40feef3d8
kavya.pl <kavya.pl@gmail.com>
2019-01-19 15:50:46 +0530

    Cool

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,56 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
+import { ReactiveFormsModule, FormBuilder } from "@angular/forms";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
+  let myObj;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, ReactiveFormsModule],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  // it("should create the app", () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app).toBeTruthy();
+  // });
+
+  // it(`should have as title 'test-tutz'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual("test-tutz");
+  // });
+
+  // it("should render title in a h1 tag", () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector("h1").textContent).toContain(
+  //     "Welcome to test-tutz!"
+  //   );
+  // });
+
+  beforeEach(() => {
+    myObj = new AppComponent(new FormBuilder());
+  });
+  it("TESTing add Method", () => {
+    myObj.x = 2;
+    myObj.add();
+    expect(myObj.x).toBe(3); 
   });
 
-  it(`should have as title 'test-tutz'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('test-tutz');
+  it("ADD USers method", () => {
+    const initialLength = myObj.users.length;
+    myObj.addUsers("Raj");
+
+    expect(myObj.users.length).toBe(initialLength + 1);
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to test-tutz!');
+  it("Validator form NAme control", () => {
+    myObj.myF.get("name").setValue("");
+    expect(myObj.myF.get("name").valid).toBeFalsy();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'test-tutz';
+  title = "test-tutz";
+  x = 0;
+  myF;
+  constructor(private formB: FormBuilder) {
+    this.myF = this.formB.group({
+      name: ["", [Validators.required]],
+      age: [],
+      habits: this.formB.group([])
+    });
+  }
+  users = [];
+  add() {
+    this.x = this.x + 1;
+  }
+
+  sub() {
+    this.x = this.x - 1;
+  }
+
+  addUsers(x) {
+    this.users.push(x);
+  }
 }
